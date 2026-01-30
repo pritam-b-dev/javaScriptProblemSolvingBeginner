1b648abf418fcd238c3b846f076cf5dea87d6daf
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-01-30 21:29:41 +0600

    break

--- break/squareBreak.js
+++ break/squareBreak.js
@@ -1,3 +1,9 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+for (i = 1; i <= 100; i++) {
+  if (i !== 1 && Math.sqrt(i) % 1 === 0) {
+    break;
+  }
+  console.log(i);
+}
---
+++ break/sumUpTo100.js
@@ -0,0 +1,15 @@
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+let i = 1;
+let sum = 0;
+while (i <= 50) {
+  console.log(i);
+  sum = sum + i;
+  if (sum > 100) {
+    break;
+  }
+  i++;
+}
+console.log("if we sum all above the numbers, we will get:", sum);
